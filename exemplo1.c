c9cf5155d1b050b9d6b7c6038666373c4bc862bc
Reiss <reissjoao28@gmail.com>
2026-02-11 19:59:43 -0300

    Create exemplo1.c
    Add a simple C program that prints messages to the screen.

---
+++ exemplo1.c
@@ -0,0 +1,14 @@
+/* lab. 1
+  * Exemplo 1*/
+
+#include <stdio.h>
+
+int main(){
+  //imprime algumans mensagens na tela
+  printf("Mackenzie - FCI/n");
+  printf("Algoritmos e ");
+  printf("Prog. II/n");
+
+
+  return 0;
+}
